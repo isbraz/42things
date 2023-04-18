132a54fa178bb5015d86c22d32dc0fe250a36cee
IsmaelBrazdaSilva <ismael.braz03@hotmail.com>
2023-04-18 12:22:54 +0100

    libft

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -6,16 +6,85 @@
 /*   By: isbraz-d <isbraz-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 17:35:41 by isbraz-d          #+#    #+#             */
-/*   Updated: 2023/04/17 18:00:33 by isbraz-d         ###   ########.fr       */
+/*   Updated: 2023/04/18 12:15:37 by isbraz-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+
+static void	ft_reverse_string(char *s1, size_t size)
+{
+	int	i;
+	int	f;
+	int	aux;
+
+	i = 0;
+	f = size - 1;
+	while (i < (size / 2))
+	{
+		aux = s1[i];
+		s1[i] = s1[f];
+		s1[f] = aux;
+		i++;
+		f--;
+	}
+}
 
 char	*ft_itoa(int n)
 {
 	char	*str;
-	if (n > 0)
-		str[0] = '-';
-	
+	int	number;
+	int	digit;
+	int	i;
+
+	i = 0;
+	number = n;
+	while (number > 0)
+	{
+		number /= 10;
+		i++;
+	}
+	str = (char *)malloc(sizeof(char) * i + 1);
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	number = n;	
+	while (number > 0)
+	{
+		digit = number % 10;
+		str[i++] = digit + 48;
+		number /= 10;
+	}
+	str[i] = '\0';
+	ft_reverse_string(str, i);
+	return (str);
+}
+
+char	*ft_itoa_teste(int n)
+{
+	char	*str;
+	size_t	number;
+	size_t	digit;
+	size_t	i;
+
+	i = 0;
+	number = n;
+	while (number > 0)
+	{
+		digit = number % 10;
+		str[i++] = digit + 48;
+		number /= 10;
+	}
+	str = (char *)malloc(sizeof(char) * i + 1);
+	str[i] = '\0';
+	ft_reverse_string(str, i);
+	return (str);
+}
+
+int	main(void)
+{
+	int	number = 2020;
+	printf("%s\n", ft_itoa_teste(number));
+	printf("%s\n", ft_itoa(number));
 }
--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,11 +6,12 @@
 /*   By: isbraz-d <isbraz-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/16 16:05:46 by isbraz-d          #+#    #+#             */
-/*   Updated: 2023/04/17 15:15:10 by isbraz-d         ###   ########.fr       */
+/*   Updated: 2023/04/17 21:01:35 by isbraz-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
@@ -38,10 +39,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	return (str);
 }
 
-/*int main(void)
+int main(void)
 {
-	char *s1 = "Hello, ";
-	char *s2 = "world!";
+	char *s1 = "Bruna ";
+	char *s2 = "Braz";
 	char *result = ft_strjoin(s1, s2);
 	
 	if (result == NULL)
@@ -49,7 +50,9 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		printf("Error: failed to allocate memory.\n");
 		return (1);
 	}
-	printf("%s\n", result);
+	printf("%p\n", s1);
+	printf("%p\n", s2);
+	//printf("%s\n", result);
 	free(result);
 	return (0);
-}*/
+}
