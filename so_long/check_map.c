fa0154fb85a1a3887affdc6166f913dda7064b16
Ismael Braz Da Silva <isbraz-d@c2r7s3.42lisboa.com>
2023-08-29 15:55:03 +0100

    working

--- so_long/check_map.c
+++ so_long/check_map.c
@@ -6,7 +6,7 @@
 /*   By: isbraz-d <isbraz-d@student.com.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 16:10:50 by isbraz-d          #+#    #+#             */
-/*   Updated: 2023/08/28 14:52:47 by isbraz-d         ###   ########.fr       */
+/*   Updated: 2023/08/28 17:23:44 by isbraz-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,15 @@ static int	ft_first_and_last_line(t_map map)
 {
 	int	i;
 	int	j;
+	int	l;
 
 	i = 0;
 	j = 0;
-	while (map.map[0][j] == '1' && map.map[0][j] != '\n')
+	while (map.map[0][j] == '1')
 		j++;
-	while (map.map[map.nlines - 1][i] == '1')
+	while (map.map[map.nlines - 2] && map.map[map.nlines - 2][i] == '1')
 		i++;
-	if (map.map[0][j] == '\n' && (map.map[map.nlines - 1][i] == '\n' 
-		|| map.map[map.nlines - 1][i] == '\0'))
+	if (j == (ft_strlen(map.map[0]) - 1) && (i == l || i == l - 1))
 		return (1);
 	return (0);
 }
@@ -33,7 +33,7 @@ static int	ft_check_content(t_map map)
 {
 	static int	i;
 	int	j;
-	
+
 	j = 0;
 	while (map.map[i][j] != '\0' && map.map[i] != NULL)
 	{
@@ -66,7 +66,7 @@ static int	ft_check_position(t_map map)
 		i++;
 	while (map.map[j][ft_strlen(map.map[j]) - 2] == '1')
 		j++;
-	if (map.map[i] == NULL && map.map[j] == NULL)
+	if (map.map && map.map[i] == NULL && map.map[j] == NULL)
 		return (1);
 	return (0);
 }
@@ -77,19 +77,25 @@ static int	ft_is_line_equal(t_map map)
 	int	j;
 
 	i = 0;
-	j = ft_strlen(map.map[i]);
+	j = ft_strlen(map.map[0]);
 	while (ft_strlen(map.map[i]) == j)
 		i++;
-	if (map.map[i] == NULL)
+	if (map.map[i] == NULL || ft_strlen(map.map[i - 1]) == j)
 		return (1);
 	return (0);
 }
 
 int	ft_map_validations(t_map map)
 {
-	if (ft_is_line_equal(map) && ft_first_and_last_line(map)
-		&& ft_check_position(map) && ft_check_content(map))
-		return (1);
+	//ft_is_line_equal(map);
+	//ft_first_and_last_line(map);
+	 if (ft_is_line_equal(map) && ft_first_and_last_line(map)
+	 	&& ft_check_position(map) && ft_check_content(map))
+	 	return (1);
+	printf("is line equal %d\n", ft_is_line_equal(map));
+	printf("first and last line %d\n", ft_first_and_last_line(map));
+	printf("check position %d\n", ft_check_position(map));
+	printf("check content %d\n", ft_check_content(map));
 	ft_putstr_fd("error", 2);
 	return (0);
 }
--- so_long/libft/ft_strlen.c
+++ so_long/libft/ft_strlen.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: isbraz-d <isbraz-d@student.42.fr>          +#+  +:+       +#+        */
+/*   By: isbraz-d <isbraz-d@student.com.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 18:06:32 by isbraz-d          #+#    #+#             */
-/*   Updated: 2023/04/17 14:11:50 by isbraz-d         ###   ########.fr       */
+/*   Updated: 2023/08/28 16:03:07 by isbraz-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ size_t	ft_strlen(const char *s)
 	size_t	i;
 
 	i = 0;
+	if (s == NULL)
+		return (0);
 	while (s[i] != '\0')
 		i++;
 	return (i);
