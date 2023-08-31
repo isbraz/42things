4842cc0de1e1ed1c2157f3a2657477aa7a4f34c9
IsmaelBrazdaSilva <ismael.braz03@hotmail.com>
2023-08-31 12:27:03 +0100

    working

--- so_long/check_map.c
+++ so_long/check_map.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   check_map.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: isbraz-d <isbraz-d@student.com.fr>         +#+  +:+       +#+        */
+/*   By: isbraz-d <isbraz-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 16:10:50 by isbraz-d          #+#    #+#             */
-/*   Updated: 2023/08/28 17:23:44 by isbraz-d         ###   ########.fr       */
+/*   Updated: 2023/08/31 12:22:20 by isbraz-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,11 @@ static int	ft_first_and_last_line(t_map map)
 	j = 0;
 	while (map.map[0][j] == '1')
 		j++;
-	while (map.map[map.nlines - 2] && map.map[map.nlines - 2][i] == '1')
+	while (map.map[map.nlines - 1] && map.map[map.nlines - 1][i] == '1')
 		i++;
-	if (j == (ft_strlen(map.map[0]) - 1) && (i == l || i == l - 1))
+	if (j == (ft_strlen(map.map[0]) - 1) \
+	&& (i == ft_strlen(map.map[map.nlines - 2]) \
+	|| i == ft_strlen(map.map[map.nlines - 1])))
 		return (1);
 	return (0);
 }
@@ -32,10 +34,10 @@ static int	ft_first_and_last_line(t_map map)
 static int	ft_check_content(t_map map)
 {
 	static int	i;
-	int	j;
+	int		j;
 
 	j = 0;
-	while (map.map[i][j] != '\0' && map.map[i] != NULL)
+	while (map.map[i] && map.map[i] != NULL && map.map[i][j] != '\0')
 	{
 		if (map.map[i][j] == 'P')
 			map.p++;
@@ -59,14 +61,17 @@ static int	ft_check_position(t_map map)
 {
 	int	i;
 	int	j;
+	int	l;
 
 	i = 0;
-	j = 0; 
-	while (map.map[i][0] == '1')
+	j = 0;
+	l = map.nlines - 1;
+	while (map.map[i] && map.map[i][0] == '1')
 		i++;
-	while (map.map[j][ft_strlen(map.map[j]) - 2] == '1')
+	while (map.map[j] && (map.map[j][ft_strlen(map.map[j]) - 2] == '1' 
+		|| map.map[j][ft_strlen(map.map[j]) - 1] == '1'))
 		j++;
-	if (map.map && map.map[i] == NULL && map.map[j] == NULL)
+	if (i == l && j == l)
 		return (1);
 	return (0);
 }
@@ -87,15 +92,11 @@ static int	ft_is_line_equal(t_map map)
 
 int	ft_map_validations(t_map map)
 {
-	//ft_is_line_equal(map);
-	//ft_first_and_last_line(map);
-	 if (ft_is_line_equal(map) && ft_first_and_last_line(map)
+	ft_is_line_equal(map);
+	ft_first_and_last_line(map);
+	if (ft_is_line_equal(map) && ft_first_and_last_line(map)
 	 	&& ft_check_position(map) && ft_check_content(map))
 	 	return (1);
-	printf("is line equal %d\n", ft_is_line_equal(map));
-	printf("first and last line %d\n", ft_first_and_last_line(map));
-	printf("check position %d\n", ft_check_position(map));
-	printf("check content %d\n", ft_check_content(map));
-	ft_putstr_fd("error", 2);
+	ft_putstr_fd("error\n", 2);
 	return (0);
 }
--- so_long/main.c
+++ so_long/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: isbraz-d <isbraz-d@student.com.fr>         +#+  +:+       +#+        */
+/*   By: isbraz-d <isbraz-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 20:49:36 by isbraz-d          #+#    #+#             */
-/*   Updated: 2023/08/28 14:49:57 by isbraz-d         ###   ########.fr       */
+/*   Updated: 2023/08/31 12:04:25 by isbraz-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,9 @@
 int	main(int ac, char **argv)
 {
 	t_map	map;
+	int	i;
 
+	i = 0;
 	ft_memset(&map, 0, sizeof(t_map));
 	ft_alocate_map(&map, argv[1]);
 	ft_map_validations(map);
